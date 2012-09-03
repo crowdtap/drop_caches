63e86289aedc4935a84e29d455a84c739cb42569
Nicolas Viennot <nicolas@viennot.biz>
2012-09-03 19:39:34 -0400

    Using getopt()

--- drop_caches.c
+++ drop_caches.c
@@ -1,14 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
 
-int main(int argc, char **argv)
+void help(int exit_status)
 {
-	int fd, i;
+	printf("Usage: drop_caches [OPTIONS]... FILE...            \n"
+	       "Drop the page caches of the given files.           \n"
+	       "                                                   \n"
+	       "  -h            show help                          \n");
+	exit(exit_status);
+}
 
-	for (i = 1; i < argc; i++) {
-		fprintf(stderr, "%s... ", argv[i]);
-		fd = open(argv[i], O_RDONLY);
+void drop_caches(char **files)
+{
+	int fd;
+	char *file;
+
+	for (; *files; files++) {
+		fprintf(stderr, "%s... ", *files);
+		fd = open(*files, O_RDONLY);
 		if (fd < 0) {
 			perror("cannot open file");
 			continue;
@@ -16,11 +27,31 @@ int main(int argc, char **argv)
 
 		fdatasync(fd);
 		if (posix_fadvise(fd, 0, 0, POSIX_FADV_DONTNEED) < 0) {
-			perror("fadvise doesn't work");
+			perror("fadvise failed");
 			continue;
 		}
 		close(fd);
 		fprintf(stderr, "flushed\n");
 	}
+}
+
+int main(int argc, char **argv)
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, "hi:")) != -1) {
+		switch (opt) {
+		case 'h':
+			help(EXIT_SUCCESS);
+		default:
+			help(EXIT_FAILURE);
+		}
+	}
+
+	if (optind >= argc)
+		help(EXIT_FAILURE);
+
+	drop_caches(argv + optind);
+
 	return 0;
 }
